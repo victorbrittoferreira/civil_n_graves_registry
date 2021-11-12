843a08448a04cc1020c6156348627721d1529667
victobrittoferreira <victobrittoferreira@gmail.com>
2021-11-12 02:57:19 -0300

    up

--- decease.py
+++ decease.py
@@ -1,7 +1,9 @@
+import datetime
 class Deceased:
 
 
-    def __init__(self, name, age) -> None:
+    def __init__(self, name, age ,  ) -> None:
+
         self.name = name
 #        self.father_name = father_name
 #        self.mother_name = mother_name
@@ -13,15 +15,26 @@ class Deceased:
 #        self.death_date = death_date
 #        self.place_of_death = place_of_death
 #        self.cause_of_death = cause_of_death 
+#        self.burial_date = burial_date
+        self.date_of_cemetery_registration = datetime.datetime.today()
 
 #        self.registry_office = registry_office
-        
+        self.dead_status = []
 
     def setNome(self, nome):
         self.nome = nome
 
     def setAge(self, age):
         self.age = age
+
+    def dead_status_update(self, updating):
+        #now = datetime.datetime.today()
+        updating_dated = (updating, datetime.datetime.today())
+        self.dead_status.append(updating_dated)
+
+    def status_records(self):
+        for update in self.dead_status:
+            print(update)
         
     
     def getName(self):
