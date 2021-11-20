340b0394ea8a5e41a0b1ae188da4e5570ceef203
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2021-11-19 23:52:44 -0300

    grave update

--- grave.py
+++ grave.py
@@ -1,3 +1,10 @@
 class Grave:
 
     def __init__(self, block, set, number, id, type, status, size ) -> None:
+        self.block = block
+        self.set = set
+        self.number = number
+        self.id = id
+        self.type = type
+        self.status = status
+        self.size = size
