1614bd77fe552eff1aa078fa4e03c6fdbea5a0d9
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2021-11-23 01:48:57 -0300

    undertaker update

---
+++ undertaker.py
@@ -0,0 +1,37 @@
+from deceased import Deceased 
+from obituary import Obituary
+from grave import Grave as make_grave
+
+
+#obituary = Obituary("victor", 30 , "SRAG, IRA")
+class Undertaker:
+
+
+    def make_grave():
+        id =+ 1
+        #grave = Grave(id,"N", 15, 2 , "brickwork", "Empty", "XG")
+        #grave = Grave()
+        #return grave
+        #print(grave.type)
+
+
+    def bury():
+        deceased_test.buried = False
+        return deceased_test.buried
+
+    def exhume_checkout_cemitery():
+        deceased_test.exhume = False
+
+    def exhume_to_numbered_ossuary():
+
+        pass
+    def exhume_to_general_ossuary():
+        pass
+
+    def judicial_exhume():
+        pass
+
+    def administrative_exhume():
+        pass
+
+
