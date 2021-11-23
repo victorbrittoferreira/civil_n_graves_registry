d78dacdded03d5ae4423c2fa5241d8cfcdf7ca19
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2021-11-22 23:53:48 -0300

    decease attributes update

--- deceased.py
+++ deceased.py
@@ -1,17 +1,14 @@
 import datetime
+
+from obituary import Obituary
 class Deceased:
 
 
-    def __init__(self, name, age , buried = bool ) -> None:
-
+    def __init__(self , obituary, buried = bool ) -> None:
+        self.obituary = obituary
         self.buried = buried
         self.dead_status = []
 
-    def setNome(self, nome):
-        self.nome = nome
-
-    def setAge(self, age):
-        self.age = age
 
     def dead_status_update(self, updating):
         #now = datetime.datetime.today()
@@ -22,11 +19,11 @@ class Deceased:
         for update in self.dead_status:
             print(update)
         
-    def getName(self):
-        return self.name
-
-    def getAge(self): 
-        return self.age 
+#    def getName(self):
+#        return self.name
+#
+#    def getAge(self): 
+#        return self.age 
 
 #deceased_test = Deceased("victor",30)
 
